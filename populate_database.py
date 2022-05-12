93e358fd3fd662dd5f7e1ac445864185ebd993a2
Casey Lam <casey_lam@berkeley.edu>
2022-05-12 14:04:45 -0700

    minor changes

--- populate_database.py
+++ populate_database.py
@@ -1,17 +1,15 @@
-
 import query_alerts
 
-# query_alerts.get_moa_lightcurves(2022)
-# query_alerts.get_ogle_lightcurves(2019)
-# query_alerts.get_kmtnet_lightcurves(2022)
+query_alerts.get_moa_lightcurves(2022)
+print('Downloaded MOA photometry from 2022 to database.')
+query_alerts.get_ogle_lightcurves(2019)
+print('Downloaded OGLE photometry from 2019 to database.')
+query_alerts.get_kmtnet_lightcurves(2022)
+print('Downloaded KMTNet photometry from 2022 to database.')
 
-# query_alerts.get_moa_alerts(2022)
-# query_alerts.get_ogle_alerts(2019)
-# query_alerts.get_kmtnet_alerts(2022)
-
-query_alerts.get_kmtnet_alerts(2021)
-query_alerts.get_kmtnet_alerts(2020)
-query_alerts.get_kmtnet_alerts(2019)
-query_alerts.get_kmtnet_alerts(2018)
-query_alerts.get_kmtnet_alerts(2017)
-query_alerts.get_kmtnet_alerts(2016)
+query_alerts.get_moa_alerts(2022)
+print('Downloaded MOA alerts from 2022 to database.')
+query_alerts.get_ogle_alerts(2019)
+print('Downloaded OGLE alerts from 2019 to database.')
+query_alerts.get_kmtnet_alerts(2022)
+print('Downloaded KMTNet alerts from 2022 to database.')
