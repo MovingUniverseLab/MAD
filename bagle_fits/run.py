5157ea3f8c12dac292eeee12098eb1c2b49bc803
Anette Brecko <anetteb@laser.astro.berkeley.edu>
2024-05-01 23:39:39 -0700

    working on adding chi2 calculation to to run.py

--- bagle_fits/run.py
+++ bagle_fits/run.py
@@ -11,6 +11,7 @@ import os
 import sys
 import pylab as plt
 import numpy as np
+import pandas as pd
 from datetime import date, datetime
 import json
 from glob import glob
@@ -97,6 +98,13 @@ def run_all():
     query_output = json.load(open(sorted(glob('query_output*'))[-1]))
     target_list = list(query_output['ra'].keys())
     for target in target_list:
+        if target[0] == 'O':
+            alert_sys = "OGLE"
+        if target[0] == 'K':
+            alert_sys = "KMTNet"
+        if target[0] == 'M':
+            alert_sys = "MOA"
+        data = pd.read_csv("lightcurves/" + alert_sys + "/" + target + ".csv")
         run_bagle(target, list(query_output['data_sets'][target].keys()), modstr)
 
 def run_one(target):
