4c6ce8db04a7ebcc7ba305632396fc122b0107ca
Macy Huston <mhuston@laser.astro.berkeley.edu>
2024-05-22 11:49:08 -0700

    fix ipython vs python issue

--- populate_database.py
+++ populate_database.py
@@ -1,18 +1,23 @@
 import query_alerts
 
-# Get alerts
-#query_alerts.get_moa_alerts(2023)
-#print('Downloaded MOA alerts from 2023 to database.')
-df1 = query_alerts.get_ogle_alerts(2024)
-print('Downloaded OGLE alerts from 2024 to database.')
-query_alerts.get_kmtnet_alerts(2024)
-print('Downloaded KMTNet alerts from 2024 to database.')
+def run(year=2024,sources=['OGLE','KMTNet']):
+    if 'MOA' in sources:
+        query_alerts.get_moa_alerts(year)
+        print('Downloaded MOA alerts from '+str(year)+' to database.')
+    if 'OGLE' in sources:
+        df1 = query_alerts.get_ogle_alerts(year)
+        print('Downloaded OGLE alerts from '+str(year)+' to database.')
+    if 'KMTNet' in sources:
+        query_alerts.get_kmtnet_alerts(year)
+        print('Downloaded KMTNet alerts from '+str(year)+' to database.')
 
-# Get light curves
-#query_alerts.get_moa_lightcurves(2023)
-#print('Downloaded MOA photometry from 2023 to database.')
-#query_alerts.get_ogle_lightcurves(2023)
-#print('Downloaded OGLE photometry from 2023 to database.')
-#query_alerts.get_kmtnet_lightcurves(2023)
-#print('Downloaded KMTNet photometry from 2023 to database.')
+    # Get light curves
+    #query_alerts.get_moa_lightcurves(2023)
+    #print('Downloaded MOA photometry from 2023 to database.')
+    #query_alerts.get_ogle_lightcurves(2023)
+    #print('Downloaded OGLE photometry from 2023 to database.')
+    #query_alerts.get_kmtnet_lightcurves(2023)
+    #print('Downloaded KMTNet photometry from 2023 to database.')
 
+if __name__ == '__main__':
+    run()
